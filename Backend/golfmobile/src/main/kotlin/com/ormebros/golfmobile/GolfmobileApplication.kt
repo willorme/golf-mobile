d9ee829608bfadf8bdb883ed7ef516859d4140fe
willorme <wjorme98@gmail.com>
2025-06-08 16:57:59 -0700

    add flutter and spring boot projects

---
+++ Backend/golfmobile/src/main/kotlin/com/ormebros/golfmobile/GolfmobileApplication.kt
@@ -0,0 +1,11 @@
+package com.ormebros.golfmobile
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GolfmobileApplication
+
+fun main(args: Array<String>) {
+	runApplication<GolfmobileApplication>(*args)
+}
---
+++ Backend/golfmobile/src/test/kotlin/com/ormebros/golfmobile/GolfmobileApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ormebros.golfmobile
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GolfmobileApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
