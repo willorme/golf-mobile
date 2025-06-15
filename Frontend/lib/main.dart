646933bfa8ad4c4224d8e29f9552158edc609f70
willorme <wjorme98@gmail.com>
2025-06-15 15:38:40 -0700

    getting started

--- Frontend/lib/main.dart
+++ Frontend/lib/main.dart
@@ -28,9 +28,9 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Golf Match'),
     );
   }
 }
